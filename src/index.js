d046caf123aed5d8976b91d857e2a6fc51d3aac4
Hammad Umar <hammadhawc227@gmail.com>
2022-02-11 07:10:03 -0800

    add html website functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import attack from './attack';
 
-console.log(attack('Smelly Ogre', 23));
+// Selecting DOM Elements
+const creature = document.querySelector('#creature');
+const damage = document.querySelector('#damage');
+const resultButton = document.querySelector('#resultButton');
+const resultTag = document.querySelector('#resultTag');
+
+resultButton.addEventListener('click', () => {
+  const result = attack(creature.value, damage.value);
+  resultTag.innerHTML = result;
+});
